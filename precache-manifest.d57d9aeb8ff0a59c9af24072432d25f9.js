ab29846ff233016b7119d0989d8a814bec0efd8a
Jayr Motta <jayrmotta@gmail.com>
2018-10-28 19:31:48 +0200

    Updates

---
+++ precache-manifest.d57d9aeb8ff0a59c9af24072432d25f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1e880ea8ea415ad3dd6",
+    "url": "/crypto-fees-calculator/static/css/main.fd8634ce.chunk.css"
+  },
+  {
+    "revision": "f1e880ea8ea415ad3dd6",
+    "url": "/crypto-fees-calculator/static/js/main.f1e880ea.chunk.js"
+  },
+  {
+    "revision": "efe36d570ce81c50ad4f",
+    "url": "/crypto-fees-calculator/static/css/1.997ef8f1.chunk.css"
+  },
+  {
+    "revision": "efe36d570ce81c50ad4f",
+    "url": "/crypto-fees-calculator/static/js/1.efe36d57.chunk.js"
+  },
+  {
+    "revision": "21dbdd1660f91c95e56e",
+    "url": "/crypto-fees-calculator/static/js/runtime~main.21dbdd16.js"
+  },
+  {
+    "revision": "a033acee4f5da1a8791c37e69562e40d",
+    "url": "/crypto-fees-calculator/index.html"
+  }
+];
